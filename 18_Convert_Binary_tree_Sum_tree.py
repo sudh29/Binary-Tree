aeff5bc7a221a16032740544b8af97730ce0cc2b
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2024-01-06 19:38:30 +0530

    Update 18_Convert_Binary_tree_Sum_tree.py

--- 18_Convert_Binary_tree_Sum_tree.py
+++ 18_Convert_Binary_tree_Sum_tree.py
@@ -15,3 +15,28 @@ class Solution:
         return old_root_data + root.data
             
   
+class Solution:
+    def sum_child(self,node):
+        left_sum = node.left.data if node.left else 0
+        right_sum = node.right.data if node.right else 0
+        return left_sum + right_sum
+    
+    def toSumTree(self, root) :
+        #code here
+        if root is None:
+            return 0
+        old_val = root.data
+        left_sum = self.toSumTree(root.left)
+        right_sum = self.toSumTree(root.right)
+        root.data = left_sum + right_sum
+        return root.data + old_val
+        
+        # if root is None:
+        #     return
+        # q=[root]
+        # while q:
+        #     curr = q.pop(0)
+        #     old_val = curr.data
+        #     curr.data = self.sum_child(curr)
+        #     if curr.left: q.append(curr.left)
+        #     if curr.right: q.append(curr.right)
